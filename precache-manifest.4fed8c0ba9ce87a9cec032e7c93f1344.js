333ad42e73ccc9d95f88e3a6c75d0076c0544103
Daniel Sundholm <daniel.j.sundholm@utu.fi>
2020-01-26 02:27:31 +0200

    Initial deployment commit

---
+++ precache-manifest.4fed8c0ba9ce87a9cec032e7c93f1344.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300dc26b5a4b202166dc3ecd82fc9ed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0b04c1897e854533d9",
+    "url": "/static/css/main.fb7b9138.chunk.css"
+  },
+  {
+    "revision": "ff3dc64f231a8ac09b09",
+    "url": "/static/js/2.48f1fc08.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.48f1fc08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb0b04c1897e854533d9",
+    "url": "/static/js/main.b0c4fa21.chunk.js"
+  },
+  {
+    "revision": "803bba5966a1601bcac1",
+    "url": "/static/js/runtime-main.b987a08d.js"
+  },
+  {
+    "revision": "8d5cac2e31ba92524f4ce959b69afd75",
+    "url": "/static/media/chatapp.8d5cac2e.jpg"
+  },
+  {
+    "revision": "baac82309ac39018d0b33c67b30b5243",
+    "url": "/static/media/fullstackopen2019img.baac8230.jpg"
+  },
+  {
+    "revision": "a8e8d7ef0b50403c24b25b199ae750c9",
+    "url": "/static/media/ilmoplaseimg.a8e8d7ef.jpg"
+  },
+  {
+    "revision": "4d3217c96746e95c38da13ab1cf0c436",
+    "url": "/static/media/pp.4d3217c9.jpg"
+  }
+]);
